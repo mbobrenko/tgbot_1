7c4cb4fff20d6b103ccc41ea88aa998c7a272abb
mbobrenko <66368910+mbobrenko@users.noreply.github.com>
2023-07-18 15:52:32 +0300

    Update telegrambot.py

--- telegrambot.py
+++ telegrambot.py
@@ -6,7 +6,7 @@ from spy import*
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hi {update.effective_user.first_name}')
 
-app = ApplicationBuilder().token("6348983717:AAGCdWgLIh5MAsTqmlb1jVtxP_9PKEsIdqE").build()
+app = ApplicationBuilder().token("E").build()
 print ('server srart')  
 app.add_handler(CommandHandler("hi", hi_command))
 app.add_handler(CommandHandler("time", time_command))
