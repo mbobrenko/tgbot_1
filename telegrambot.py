d019182886c2c9ec577cd970e9eeb658868e2d02
Marina Bobrenko <m.bobrenko@gmail.com>
2023-07-18 15:38:03 +0300

    tg bot

---
+++ telegrambot.py
@@ -0,0 +1,16 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from bots_command import*
+from spy import*
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hi {update.effective_user.first_name}')
+
+app = ApplicationBuilder().token("6348983717:AAGCdWgLIh5MAsTqmlb1jVtxP_9PKEsIdqE").build()
+print ('server srart')  
+app.add_handler(CommandHandler("hi", hi_command))
+app.add_handler(CommandHandler("time", time_command))
+app.add_handler(CommandHandler("help", help_command))
+app.add_handler(CommandHandler("sum", sum_command))
+
+app.run_polling()
